1e8fc0872ad7c5cf94b6c2b76e8dc025a4b2cddc
Francis <fnkorley@gmail.com>
2025-11-16 16:14:54 +0000

    Fix: Added required imports, updated templates, added function+class URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
-from .views import LibraryDetailView
+from .views import list_books, ListBooksView, LibraryDetailView
 
 urlpatterns = [
-    path("books/", views.list_books, name="list_books"),
-    path("library/<int:pk>/", LibraryDetailView.as_view(), name="library_detail"),
+    path('books/', ListBooksView.as_view(), name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('books-function/', list_books, name='books_function'),
 ]
