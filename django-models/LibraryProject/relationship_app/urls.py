a7b319f4519f91d54659bd4a90ae4feb6c51c196
Francis <fnkorley@gmail.com>
2025-11-16 16:20:12 +0000

    Fixed all required patterns for autograder

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -3,6 +3,6 @@ from .views import list_books, ListBooksView, LibraryDetailView
 
 urlpatterns = [
     path('books/', ListBooksView.as_view(), name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
     path('books-function/', list_books, name='books_function'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Book, Library
 
+# Function-based view required by the task
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, "relationship_app/list_books.html", {"books": books})
+
+# Class-based ListView (also satisfies the “simple text list” requirement)
 class ListBooksView(ListView):
     model = Book
     template_name = "relationship_app/list_books.html"
     context_object_name = "books"
 
-    def get_queryset(self):
-        return Book.objects.all()  # REQUIRED BY GRADER
-
-
+# Class-based DetailView for Library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
