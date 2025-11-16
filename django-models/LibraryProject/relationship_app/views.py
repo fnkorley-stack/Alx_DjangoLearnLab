a824d3ac3bd89d9d74a71dcf2b21a66366ddd7bd
Francis <fnkorley@gmail.com>
2025-11-16 19:48:28 +0000

    Fixed import line for autograder

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
-# Function-based view required by the task
 def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based ListView (also satisfies the “simple text list” requirement)
+
 class ListBooksView(ListView):
     model = Book
     template_name = "relationship_app/list_books.html"
     context_object_name = "books"
 
-# Class-based DetailView for Library
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
